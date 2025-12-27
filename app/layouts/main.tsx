33f215ec52c18cdc7d367fdcddcce87d8fcd4493
jigangjon <jigangjon@gmail.com>
2025-12-27 14:10:42 +0900

    implemented ui for teacher dashboard

--- app/layouts/main.tsx
+++ app/layouts/main.tsx
@@ -2,11 +2,13 @@ import { Outlet } from "react-router";
 
 export default function Main() {
   return (
-    <>
-      <header>Bero</header>
+    <div className="min-h-screen">
+      <header className="flex items-center bg-[#90baff] h-24 w-full text-white text-7xl pl-6">
+        BeRO
+      </header>
       <main>
         <Outlet />
       </main>
-    </>
+    </div>
   );
 }
